775bf1018cf945ff045dfee97a03280418618f27
wangxu2015 <wangxu@kongkonghu.com>
2016-10-31 17:28:07 +0800

    下载文件和预览文件功能完善 文件操作异常捕获添加 添加日志打印

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,17 @@ var upload = multer();
 var mkdirp = require('mkdirp');
 
 /**
+ * 创建静态资源存放文件夹
+ * 
+ */
+if(!fs.existsSync("../uploads")) {
+	logger.info("---静态资源存放文件夹不存在---");
+	logger.info("---正在创建静态资源存放文件夹---");
+	mkdirp.sync("../uploads");
+	logger.info("---创建静态资源存放文件夹成功---")
+}
+
+/**
  * 路由统一处理分发
  * 
  */
@@ -46,11 +57,18 @@ router.get('/', function(req, res, next) {
  */
 router.get('/own-oss-pre/:name', function(req, res) {
 	var name = req.params.name;
-	logger.info('进入静态文件请求：' + name);
+	logger.info('---', name, '文件预览开始---');
 	fs.readFile('../uploads/' + name, function(err, data) {
-		if(err) throw err;
-		res.write(data);
-		res.end()
+		if(err) {
+			logger.error('--- ', name, ' 文件预览读取出错：', name, "---");
+			logger.error('--- ', name, ' 错误信息---\n');
+			logger.error(err);
+			logger.error('--- ', name, ' 错误信息结束---\n');
+		} else {
+			res.write(data);
+			res.end()
+			logger.info('---', name, '文件预览成功返回---');
+		}
 	});
 });
 
@@ -60,9 +78,19 @@ router.get('/own-oss-pre/:name', function(req, res) {
  */
 router.get('/own-oss-download/:name', function(req, res) {
 	var name = req.params.name;
-	logger.info('进入静态文件请求：' + name);
-	res.attachment('../uploads/' + name);
-	res.end()
+	logger.info('--- ', name, ' 文件下载开始---');
+	fs.access('../uploads/' + name, function(err, data) {
+		if(err) {
+			logger.error('--- ', name, ' 文件下载出错：', name, "---");
+			logger.error('--- ', name, ' 错误信息---\n');
+			logger.error(err);
+			logger.error('--- ', name, ' 错误信息结束---\n');
+		} else {
+			res.attachment('../uploads/' + name);
+			res.end()
+			logger.info('--- ', name, ' 文件下载成功返回---');
+		}
+	});
 });
 
 /**
@@ -71,14 +99,17 @@ router.get('/own-oss-download/:name', function(req, res) {
  */
 router.post("/uploadFile", upload.array('file', 12), function(req, res) {
 	if(!fs.existsSync("../uploads")) {
+		logger.info("---静态资源存放文件夹不存在---");
+		logger.info("---正在创建静态资源存放文件夹---");
 		mkdirp.sync("../uploads");
+		logger.info("---创建静态资源存放文件夹成功---")
 	}
-	logger.info("---正在上传文件---")
+	logger.info("---正在上传文件---");
 	var data = {};
 	data.code = "000000";
 	data.msg = "success";
 	var len = req.files.length;
-	logger.info("---上传文件数量：" + len, "个---")
+	logger.info("---上传文件数量：" + len, "个---");
 	_.each(req.files, function(file) {
 		fs.writeFile("../uploads/" + file.originalname, file.buffer, function(err) {
 			len--;
@@ -91,7 +122,7 @@ router.post("/uploadFile", upload.array('file', 12), function(req, res) {
 				logger.info("---", file.originalname, "上传完成---");
 			}
 			if(len === 0) {
-				logger.info("---全部文件上传完成---")
+				logger.info("---全部文件上传完成---");
 				res.write(JSON.stringify(data));
 				res.end();
 			}
@@ -104,14 +135,28 @@ router.post("/uploadFile", upload.array('file', 12), function(req, res) {
  * 
  */
 router.get("/fileList", function(req, res) {
-	logger.info("---请求文件列表---")
+	if(!fs.existsSync("../uploads")) {
+		logger.info("---静态资源存放文件夹不存在---");
+		logger.info("---正在创建静态资源存放文件夹---");
+		mkdirp.sync("../uploads");
+		logger.info("---创建静态资源存放文件夹成功---")
+	}
+	logger.info("---请求文件列表---");
 	logger.info("---pageNum：" + req.query.pageNum + "---");
 	logger.info("---pageSize：" + req.query.pageSize + "---");
 	var _pageNum = req.query.pageNum || 1;
 	var _pageSize = req.query.pageSize || 12;
 	var _start = (_pageNum - 1) * _pageSize;
 	var _end = _pageNum * _pageSize - 1;
-	var _fileList = fs.readdirSync("../uploads");
+	var _fileList = [];
+	try {
+		_fileList = fs.readdirSync("../uploads");
+	} catch(e) {
+		logger.error('---获取静态文件列表 --> 读取静态资源文件夹失败---\n');
+		logger.error('---读取静态资源文件夹失败 错误信息开始---\n');
+		logger.error(e);
+		logger.error('\n---读取静态资源文件夹失败 错误信息结束---\n');
+	}
 	var data = {
 		code: "000000",
 		msg: "success",
@@ -123,11 +168,19 @@ router.get("/fileList", function(req, res) {
 		}
 		var _fileObj = new Object();
 		_fileObj.name = _fileList[i];
-		_fileObj.link = "http://120.27.158.158:1314/own-oss/" + _fileList[i];
-		var tempObj = fs.statSync("../uploads/" + _fileList[i]);
-		_fileObj.originalTime = new Date(tempObj.birthtime).getTime();
-		_fileObj.latestTime = new Date(tempObj.mtime).getTime();
-		_fileObj.size = (tempObj.size / 1024) > 1024 ? (tempObj.size / 1024 / 1024).toFixed(2) + "Mb" : (tempObj.size / 1024).toFixed(2) + "Kb";
+		_fileObj.preLink = "http://120.27.158.158:1314/own-oss-pre/" + _fileList[i];
+		_fileObj.downloadLink = "http://120.27.158.158:1314/own-oss-download/" + _fileList[i];
+		try {
+			var tempObj = fs.statSync("../uploads/" + _fileList[i]);
+			_fileObj.originalTime = new Date(tempObj.birthtime).getTime();
+			_fileObj.latestTime = new Date(tempObj.mtime).getTime();
+			_fileObj.size = (tempObj.size / 1024) > 1024 ? (tempObj.size / 1024 / 1024).toFixed(2) + "Mb" : (tempObj.size / 1024).toFixed(2) + "Kb";
+		} catch(e) {
+			logger.error('---获取 ', _fileList[i], ' 文件信息失败---\n');
+			logger.error('---获取 ', _fileList[i], ' 文件信息失败 错误信息开始---\n');
+			logger.error(e);
+			logger.error('---获取 ', _fileList[i], ' 文件信息失败 错误信息结束---\n');
+		}
 		data.list.push(_fileObj);
 	}
 	logger.info("---返回：" + JSON.stringify(data) + "---");
